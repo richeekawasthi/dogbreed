a39d29b19b36bbf562e7ff4756c1e84ec98d0d98
Richeek Awasthi <richeek.awasthi@gmail.com>
2018-10-07 18:53:52 +0530

    Final Commit

--- test.py
+++ test.py
@@ -37,7 +37,20 @@ for image in test_images:
 	img = preprocessing_fn(img)
 	img = np.array([img])
 	classes = model.predict(img, batch_size=1)
-	result[image] = np.argmax(classes[0])
+	results[image] = classes[0]
+f = open("submission.csv","w")
+out_str = "id,affenpinscher,afghan_hound,african_hunting_dog,airedale,american_staffordshire_terrier,appenzeller,australian_terrier,basenji,basset,beagle,bedlington_terrier,bernese_mountain_dog,black-and-tan_coonhound,blenheim_spaniel,bloodhound,bluetick,border_collie,border_terrier,borzoi,boston_bull,bouvier_des_flandres,boxer,brabancon_griffon,briard,brittany_spaniel,bull_mastiff,cairn,cardigan,chesapeake_bay_retriever,chihuahua,chow,clumber,cocker_spaniel,collie,curly-coated_retriever,dandie_dinmont,dhole,dingo,doberman,english_foxhound,english_setter,english_springer,entlebucher,eskimo_dog,flat-coated_retriever,french_bulldog,german_shepherd,german_short-haired_pointer,giant_schnauzer,golden_retriever,gordon_setter,great_dane,great_pyrenees,greater_swiss_mountain_dog,groenendael,ibizan_hound,irish_setter,irish_terrier,irish_water_spaniel,irish_wolfhound,italian_greyhound,japanese_spaniel,keeshond,kelpie,kerry_blue_terrier,komondor,kuvasz,labrador_retriever,lakeland_terrier,leonberg,lhasa,malamute,malinois,maltese_dog,mexican_hairless,miniature_pinscher,miniature_poodle,miniature_schnauzer,newfoundland,norfolk_terrier,norwegian_elkhound,norwich_terrier,old_english_sheepdog,otterhound,papillon,pekinese,pembroke,pomeranian,pug,redbone,rhodesian_ridgeback,rottweiler,saint_bernard,saluki,samoyed,schipperke,scotch_terrier,scottish_deerhound,sealyham_terrier,shetland_sheepdog,shih-tzu,siberian_husky,silky_terrier,soft-coated_wheaten_terrier,staffordshire_bullterrier,standard_poodle,standard_schnauzer,sussex_spaniel,tibetan_mastiff,tibetan_terrier,toy_poodle,toy_terrier,vizsla,walker_hound,weimaraner,welsh_springer_spaniel,west_highland_white_terrier,whippet,wire-haired_fox_terrier,yorkshire_terrier\n"
+for image in test_images:
+	out_str = out_str + image.split(".jpg")[0] + ","
+	output = results[image]
+	for i in range(120):
+		out_str = out_str + str(output[i])
+		if(i!=119):
+			out_str+=','
+		else:
+			out_str+='\n'
+f.write(out_str)
+f.close()
 
 '''test_generator = ImageDataGenerator(preprocessing_function=preprocessing_fn)
 test_generator = test_generator.flow_from_directory(test_dir,target_size=(299,299),batch_size=batch_size,class_mode='binary')
