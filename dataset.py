4e2f71de75cc51a1b67b1b2e022aa24bdf26875b
Richeek Awasthi <richeek.awasthi@gmail.com>
2018-10-07 15:57:51 +0530

    Third Commit

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import shutil
 
 def breed_dictionary(filename):
 	breed_dict = {}
@@ -12,18 +13,38 @@ def breed_dictionary(filename):
 
 def load_data(image_dir, labels_file, classes_file):
 	labels = np.array(pd.read_csv("labels.csv"))
-	breed_dict = breed_dictionary(classes_file)
+	#breed_dict = breed_dictionary(classes_file)
 	labels_dict = {}
 	for label in labels:
-		labels_dict[label[0]] = breed_dict[label[1]]
+		labels_dict[label[0]] = label[1]
 	images = os.listdir(image_dir)
 	labels = []
 	error = []
 	for image in images:
 		labels.append(labels_dict[image.split(".")[0]])
 	images = [os.path.join(image_dir,image) for image in images]
-	return images, labels
-
+	if not os.path.exists("training_data/"):
+		os.mkdir("training_data/")
+		os.mkdir("validation_data/")
+		for breed in list(breed_dict.keys()):
+			os.mkdir("training_data/"+breed)
+			os.mkdir("validation_data/"+breed)
+	shift_dict = {}
+	for breed in list(set(labels)):
+		shift_dict[breed] = []
+	for i in range(len(labels)):
+		shift_dict[labels[i]].append(images[i])
+	for breed in list(set(labels)):
+		#print(shift_dict[breed])
+		num_val = int(0.2*len(shift_dict[breed]))
+		for i in range(num_val):
+			shutil.copy(shift_dict[breed][i],os.path.join("validation_data",breed))
+		for i,img in enumerate(shift_dict[breed]):
+			if(i<num_val):
+				continue
+			else:
+				shutil.copy(shift_dict[breed][i],os.path.join("training_data",breed))
+		
 '''class Batcher(object):
 
 	def __init__(self, image_dir, labels_file="", classes_file, mode="TRAIN"):
--- train.py
+++ train.py
@@ -2,11 +2,21 @@ import keras
 import numpy as np
 
 def create_graph(num_classes):
-	base = keras.applications.inception_v3.InceptionV3(weights='imagenet', include_top=False, input_shape=(299,299,3), classes=120)
+	base = keras.applications.inception_v3.InceptionV3(weights='imagenet', include_top=False, input_shape=(299,299,3))
 	penultimate = keras.layers.GlobalAveragePooling2D()(base.output)
 	logits = keras.layers.Dense(num_classes, activation='softmax')(penultimate)
 	model = keras.models.Model(input=base.input, output=logits)
 	print(base.input)
 	print(logits)
 
-graph = create_graph(120)
+###################HYPERPARAMETERS#########################
+
+num_classes = 120
+learning_rate = 1e-3
+decay = 1e-6
+momentum = 0.9
+
+###########################################################
+
+
+graph = create_graph(num_classes)
